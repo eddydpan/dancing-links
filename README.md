261da3b3f9f7657b24d5b7d0e733aa5c99daf164
eddydpan <eddydotpan@gmail.com>
2025-05-05 11:33:59 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# dancing-links
-a Python implementaiton of the Dancing Links (DLX) algorithm for Sudoku
--- src/components/Legend.jsx
+++
@@ -1,9 +0,0 @@
-function Legend() {
-    return (
-        <div>
-        <h1>Legend View</h1>
-        </div>
-    );
-}
-
-export default Legend;
