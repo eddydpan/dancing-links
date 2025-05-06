d4f89c3cff3b19612df83c9f71b7a428c2ec932d
gzak25 <gzak@olin.edu>
2025-05-05 22:51:00 -0400

    Adding skeleton of the testing file for n-queens

---
+++ LeetCode/test_n_queens_solver.py
@@ -0,0 +1,27 @@
+import unittest
+from n_queens import Solution
+
+class TestNQueensSolver(unittest.Testcase):
+    def setUp(self):
+        self.solver = Solution()
+
+    def test_negative_n(self):
+        pass
+
+    def test_zero_n(self):
+        pass
+
+    def test_one_n(self):
+        pass
+
+    def test_two_n(self):
+        pass
+
+    def test_three_n(self):
+        pass
+
+    def test_four_n(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
