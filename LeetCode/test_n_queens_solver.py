10cbf36740c10688f62f229c642f65c4c7cf383a
gzak25 <gzak@olin.edu>
2025-05-06 00:40:29 -0400

    Added unit tests for n-queens

--- LeetCode/test_n_queens_solver.py
+++ LeetCode/test_n_queens_solver.py
@@ -1,27 +1,46 @@
 import unittest
 from n_queens import Solution
 
-class TestNQueensSolver(unittest.Testcase):
+class TestNQueensSolver(unittest.TestCase):
     def setUp(self):
         self.solver = Solution()
 
     def test_negative_n(self):
-        pass
+        """A negative value of n should raise and exception as that's impossible
+        """
+        with self.assertRaises(ValueError):
+            self.solver.solveNQueens(-1)
 
     def test_zero_n(self):
-        pass
+        """n = 0 should return one solution, an empty one: [[]]
+        """
+        self.assertEqual(self.solver.solveNQueens(0), [[]])
 
     def test_one_n(self):
-        pass
+        """n = 1 should return one solution, [["Q"]]
+        """
+        self.assertEqual(self.solver.solveNQueens(1), [['Q']])
 
     def test_two_n(self):
-        pass
+        """n = 2 should return no solution, an empty solutions list
+        """
+        self.assertEqual(self.solver.solveNQueens(2), [])
 
     def test_three_n(self):
-        pass
+        """n = 3 should return no solution, an empty solutions list
+        """
+        self.assertEqual(self.solver.solveNQueens(3), [])
 
     def test_four_n(self):
-        pass
+        """n = 4 should return 2 solutions:
+        [['.Q..', '...Q', 'Q...', '..Q.'], 
+        ['..Q.', 'Q...', '...Q', '.Q..']]
+        """
+        correct_solution = [
+            ['.Q..', '...Q', 'Q...', '..Q.'], 
+            ['..Q.', 'Q...', '...Q', '.Q..']]
+
+        self.assertEqual(self.solver.solveNQueens(4), correct_solution)
 
 if __name__ == '__main__':
     unittest.main()
